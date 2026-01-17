eebd07e1d272327422cedd498fc16fd4fc71a32c
munash-star <katesimunashe@gmail.com>
2026-01-18 01:37:20 +0200

    I added my name

--- putting_python_to_work.py
+++ putting_python_to_work.py
@@ -1,9 +1,25 @@
 # TO-DO: get the radius of the circle from the user
+import math
+
+
+radius=float  (input ("Enter the radius of the circle: "))
 
 # TO-DO: calculate the diameter of the circle
+diameter =2*radius
+
 
 # TO-DO: calculate the circumference of the circle
+circumference = 2 * math.pi * radius
+
 
 # TO-DO: calculate the area of the circle
+area=math.pi * radius **2
 
 # TO-DO: print the values for the user
+print("The radius is:", radius)
+
+print("The diameter of the circle is:", diameter)
+
+print("The circumference of the circle is:", circumference)
+
+print
